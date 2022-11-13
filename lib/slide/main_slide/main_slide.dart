256f59d523cb9b5c1252c245408a0767e4abfe95
Watcharapong Sribun <watcharapong_sribun@outlook.com>
2022-11-13 23:49:20 +0700

    extract widget and some adaptive UI

--- lib/slide/main_slide/main_slide.dart
+++ lib/slide/main_slide/main_slide.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:web_profile/const/image_path_const.dart';
+import 'package:web_profile/slide/main_slide/widgets/name_widget.dart';
+import 'package:web_profile/slide/main_slide/widgets/skills_widget.dart';
 
 class MainSlide extends StatefulWidget {
   const MainSlide({super.key});
@@ -18,138 +20,29 @@ class _MainSlideState extends State<MainSlide> {
           fit: BoxFit.fitHeight,
         ),
       ),
-      child: Row(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 30,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "My name is",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
+      child: LayoutBuilder(
+        builder: (_, boxConstraints) {
+          return boxConstraints.maxWidth >= 600
+              ? Row(
+                  children: const [
+                    NameWidget(),
+                    Spacer(),
+                    SkillWidget(),
+                  ],
+                )
+              : Column(
+                  children: const [
+                    NameWidget(),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: SkillWidget(),
                       ),
-                      width: 10,
-                      height: 120,
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Rapeepan",
-                        style: TextStyle(
-                          fontSize: 64,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        "Mokmoei",
-                        style: TextStyle(
-                          fontSize: 64,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Spacer(),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: const SkillCard(
-                  TextSkill: ("Flutter"),
-                  imageSkill: ("flutter.png"),
-                ),
-              ),
-              SkillCard(
-                TextSkill: ("UX/UI"),
-                imageSkill: ("uxui.png"),
-              ),
-              SkillCard(
-                TextSkill: ("Photo"),
-                imageSkill: ("photo.jpg"),
-              ),
-            ],
-          ),
-        ],
+                  ],
+                );
+        },
       ),
-    );
-  }
-}
-
-class SkillCard extends StatelessWidget {
-  final String TextSkill;
-  final String imageSkill;
-  const SkillCard({
-    required this.TextSkill,
-    required this.imageSkill,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 150,
-          height: 36.82,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Text(
-                TextSkill,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-              ),
-            ),
-          ),
-          decoration: const BoxDecoration(color: Color(0xFFC8D0B8), borderRadius: BorderRadius.all(Radius.circular(15.0))),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Container(
-            width: 150,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/' + imageSkill),
-              ),
-            ),
-          ),
-        )
-      ],
     );
   }
 }
